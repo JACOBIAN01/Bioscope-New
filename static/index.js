46d16f80cefd2503b2d9e0470e47212ef1b0697f
JACOBIAN01 <subhadeepghorai23@gmail.com>
2024-09-17 21:46:36 +0530

    update

--- static/index.js
+++ static/index.js
@@ -1,23 +1,3 @@
-
-// function DisplayMovie() {
-  
-//   const MovieList = document.getElementById("movie-container");
-
-//   movies.forEach((movie) => {
-//     const MovieDiv = document.createElement("div");
-//     MovieDiv.classList.add("movie-item");
-
-//     MovieDiv.innerHTML = `
-//         <img src=${movie.Poster} alt="${movie.Name}">
-//         <h2>${movie.Name}</h2>
-//         <a href=${movie.Link} class = "MovieLink" target="_blank"><button>Watch Now</button></a>
-//      `;
-//     MovieList.appendChild(MovieDiv);
-//   });
-// }
-
-// window.onload = DisplayMovie;
-
 fetch('/api/movies').then(response=> response.json()).then(data=>{
   const container = document.getElementById("movie-container");
 
