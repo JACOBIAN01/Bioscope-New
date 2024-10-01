21fb9308a8e3311e7d5bb1c62823ebaa6a102051
JACOBIAN01 <subhadeepghorai23@gmail.com>
2024-10-01 23:08:16 +0530

    update

--- static/index.js
+++ static/index.js
@@ -1,14 +1,20 @@
-fetch('/api/movies').then(response=> response.json()).then(data=>{
-  const container = document.getElementById("movie-container");
+const container = document.getElementById("movie-container");
 
-  data.forEach(movie=>{
-    const movieitem = `
+fetch("/api/movies")
+  .then((response) => response.json())
+  .then((data) => {
+    data.forEach((movie) => {
+      const movieitem = `
     <div class = "movie-item">
     <img src=${movie.Poster} alt="${movie.Name}">
     <h2>${movie.Name}</h2>
     <a href=${movie.Link} class = "MovieLink"  target="_blank"><button>Watch Now</button></a> 
     </div>
     `;
-    container.innerHTML +=movieitem;
-  })
-})
+      container.innerHTML += movieitem;
+    });
+  });
+
+
+
+
